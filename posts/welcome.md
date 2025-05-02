e27a5857b51e004f5495237f56dceaed980e8779
Abdullah Bilal <lordrasmussen@gmail.com>
2025-05-02 21:14:05 +0500

    first commit

---
+++ posts/welcome.md
@@ -0,0 +1,6 @@
+---
+title: "Welcome to My Microbiology Blog"
+date: 2025-05-01
+---
+
+This is your first blog post! Edit or delete it from Netlify CMS.
---
+++ script.js
@@ -0,0 +1,9 @@
+
+fetch('/posts/index.json')
+  .then(response => response.json())
+  .then(posts => {
+    const container = document.getElementById('blog-posts');
+    container.innerHTML = posts.map(post => (
+      `<article><h2>${post.title}</h2><p>${post.excerpt}</p></article>`
+    )).join('');
+  });
